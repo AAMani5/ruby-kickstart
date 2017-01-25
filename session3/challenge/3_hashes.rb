8ce198addfd54863f7cb317e0e2b0cad01d664b0
Ashwini <ashie.oh@gmail.com>
2017-01-25 15:24:42 +0000

    session 3 challenge 3 done

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,8 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+def word_count(sentence)
+  frequency = Hash.new(0)
+  sentence.split.each{|word| frequency[word.downcase] += 1}
+  frequency
+end
